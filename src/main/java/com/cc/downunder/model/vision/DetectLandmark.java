89ff9109dbe794f36dd836d21eaf7ba4ae96f098
s3763636 <s3763636@student.rmit.edu.au>
2020-05-17 21:20:06 +1000

    google vision displays on ui

--- src/main/java/com/cc/downunder/model/vision/DetectLandmark.java
+++ src/main/java/com/cc/downunder/model/vision/DetectLandmark.java
@@ -1,22 +1,24 @@
 package com.cc.downunder.model.vision;
 
 import com.google.cloud.vision.v1.*;
+import com.google.type.LatLng;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Formatter;
 import java.util.List;
 
 public class DetectLandmark {
 
     //TODO: change to your own
-    String projectID = "s3763636-myapi";
-    String bucketName = "s3763636-myapi.appspot.com";
-
+    private String projectID = "s3763636-myapi";
+    private String bucketName = "s3763636-myapi.appspot.com";
+    private StringBuilder sb = new StringBuilder();
+    private String landmarkName, landmarkLocation;
 
     public void identifyLandmark(MultipartFile file) {
         String filePath = saveFile(file);
@@ -25,13 +27,14 @@ public class DetectLandmark {
         bucket.uploadObject(projectID, bucketName, file.getOriginalFilename(), filePath);
 
         String gcsPath = "gs://" + bucketName + "/" + file.getOriginalFilename();
-        analyseImage(gcsPath, System.out);
+        analyseImage(gcsPath);
 
     }
 
-
-    public void analyseImage(String gcsPath, PrintStream out) {
+    public void analyseImage(String gcsPath) {
         List<AnnotateImageRequest> requests = new ArrayList<>();
+        sb = new StringBuilder();
+        Formatter formatter = new Formatter(sb);
 
         ImageSource imgSource = ImageSource.newBuilder().setGcsImageUri(gcsPath).build();
         Image img = Image.newBuilder().setSource(imgSource).build();
@@ -46,23 +49,33 @@ public class DetectLandmark {
 
             for (AnnotateImageResponse res : responses) {
                 if (res.hasError()) {
-                    out.printf("Error: %s\n", res.getError().getMessage());
+                    System.out.printf("Error: %s\n", res.getError().getMessage());
                     return;
                 }
 
                 // For full list of available annotations, see http://g.co/cloud/vision/docs
                 for (EntityAnnotation annotation : res.getLandmarkAnnotationsList()) {
                     LocationInfo info = annotation.getLocationsList().listIterator().next();
-                    out.printf("Landmark: %s\n %s\n", annotation.getDescription(), info.getLatLng());
+                     setImageLocationDesc(annotation.getDescription(), info.getLatLng());
+
                 }
             }
         } catch (IOException ioe) {
             System.out.println(ioe.getMessage());
         }
     }
-    private void analyseImage() {
 
+    public void setImageLocationDesc(String landmarkLocation, LatLng latLng) {
+//         sb = new StringBuilder();
+        Formatter formatter = new Formatter(sb);
+        sb.append(formatter.format("Landmark: %s\n %s\n", landmarkLocation, latLng));
     }
+
+    public String getImageLocationDesc() {
+//        System.out.println();
+         return sb.toString();
+    }
+
     private String saveFile(MultipartFile file) {
         //TODO: change to your own path
         String baseDir = "/Users/Jess/Documents/Master of IT/Workspace/DownUnder/src/main/resources/static/uploads/";
@@ -76,4 +89,5 @@ public class DetectLandmark {
         }
         return finalPath;
     }
+
 }
